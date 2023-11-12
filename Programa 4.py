b3085a40db5241dd9e068f99d708a9b360d253a7
IsabelaGT <141416765+IsabelaGT@users.noreply.github.com>
2023-11-12 18:53:16 -0300

    Add files via upload

--- Programa 4.py
+++ Programa 4.py
@@ -1,6 +1,6 @@
 """
 Nome do desenvolvedor: Isabela Garcia Tissianel
-Data: 
+Data: 28/10/2023
 Exercício: Programa 4 - Faça um Programa que leia uma lista de 10 caracteres, e diga quantas consoantes foram lidas. Imprima as consoantes.
 """
 
@@ -16,6 +16,7 @@ tam = 0
 #Lógica
 for i in range(10):
     letra = str(input("Insira uma letra: "))
+
     if not(letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u"):
         letras.append(letra)
         tam = tam + 1
@@ -23,7 +24,4 @@ for i in range(10):
 #Resultado
 print(letras)
 print("Quantidade de consoantes: ", tam)        
-
-
-
 
